d77701ccdb4542d1904be434dd476e2b3676408c
raycarbone <raycarbone@gmail.com>
2017-03-31 15:57:06 -0400

    Started the letter checking engine.

--- hang-man/bin/hang-man.py
+++ hang-man/bin/hang-man.py
@@ -12,15 +12,32 @@ def main():
     print();print('Hang-Man Game!');print()
     action = input('What would you like to do, [S]tart a new game? Or [Q]uit: ')
 
+
     if action == 'S' or action == 's':
       print(); print("Let's play...")
       new_player = input('Please enter your Name: ')
 
       # sart a new game
-      create_contact(new_name, new_email, new_phone, new_category)
+      #create_contact(new_name, new_email, new_phone, new_category)
+      A = list("This Word")
+      B = list("---------")
 
-    elif action == 'Q' or action == 'q':
-      exit(0)
+    guess = input('Enter a letter: ')
+    k = 0
+    for i in A:
+        if i == guess:
+            B[k] = i
+        k=k+1
+    print(B)
+
+
+
+
+
+
+
+    #elif action == 'Q' or action == 'q':
+    #  exit(0)
 
 
 if __name__ == '__main__':
