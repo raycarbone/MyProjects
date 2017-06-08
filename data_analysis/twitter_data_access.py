fe12533855ce793b18089a2ee327192e38e88775
raycarbone <raycarbone@gmail.com>
2017-06-08 15:18:42 -0400

    Added twitter feed analysis script

---
+++ data_analysis/twitter_data_access.py
@@ -0,0 +1,41 @@
+from tweepy import OAuthHandler, Stream
+from tweepy.streaming import StreamListener
+
+consumer_key = 'vttGSL2Lou3d1LMqnPu2c114r'
+consumer_secret = 'V2vIE9ZRLe62aqf3Ka4ds2yie1wyDathrbcjRbxmMPpGWad4f3'
+access_token =  '24293029-HDKDTcwMv76otEbtwPtafhzBthwXrf6Dg8d7lWK3x'
+access_token_secret = 'PEeh5xVHunUcnf37jxQaWyoAAiBlV8ArAsLbFgddTBh4p'
+
+auth = OAuthHandler(consumer_key,
+			consumer_secret)
+
+auth.set_access_token(access_token, access_token_secret)
+
+class PrintListener(StreamListener):
+	def on_status(self, status):
+		print(status.text)
+		prnt(status.author.screen_name,
+			status.created_at,
+			status.source,
+			'\n')
+
+	def on_error(self, status_code):
+		print("Error Code: {}".format(status_code))
+		return True # Keep stream alive
+
+	def on_timeout(self):
+		print("Listener timed out!")
+		return True # Keep stream alive
+
+
+def print_to_terminal():
+	listener = PrintListener()
+	stream = Stream(auth, listener)
+	stream.sample()
+
+if __name__=='__main__':
+	print_to_terminal
+
+
+
+
